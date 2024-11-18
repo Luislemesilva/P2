dc479aec1930a98883432f4f59a655040f56a563
Luis Eduardo Leme Silva <137091295+Luislemesilva@users.noreply.github.com>
2024-11-18 19:32:08 -0300

    user.service.ts

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -1,4 +1,3 @@
-// src/app/services/user.service.ts
 
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
@@ -12,22 +11,18 @@ export class UserService {
 
   constructor(private http: HttpClient) {}
 
-  // Método para obter a lista de usuários
   getUsers(): Observable<any[]> {
     return this.http.get<any[]>(`${this.apiUrl}/users`);
   }
 
-  // Método para obter detalhes de um usuário
   getUserById(id: number): Observable<any> {
     return this.http.get<any>(`${this.apiUrl}/users/${id}`);
   }
 
-  // Método para obter a lista de postagens
   getPosts(): Observable<any[]> {
     return this.http.get<any[]>(`${this.apiUrl}/posts`);
   }
 
-  // Método para obter detalhes de uma postagem
   getPostById(id: number): Observable<any> {
     return this.http.get<any>(`${this.apiUrl}/posts/${id}`);
   }
